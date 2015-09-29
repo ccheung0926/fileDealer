b16a6285594a97fcbeab6dc5d71a9dd5985e4b08
ccheung0926 <cindycheung0926@gmail.com>
2015-09-29 16:05:11 -0700

    [fixed] edited the create game cb

--- server/db/index.js
+++ server/db/index.js
@@ -12,16 +12,13 @@ module.exports = {
 
   createGameName: function(params, cb) {
       var gameTable = "INSERT into gameTable(game_name) VALUES(?)";
-      var treasureInfo = "INSERT into treasureInfo(gameId) VALUES(?)";
-      var playerContact = "INSERT into playerContact(gameId) VALUES(?)";
       connection.query(gameTable, params, function(err, results){
         if(err){
           console.log(err);
         }
         else{
           //gameId = results.insertId
-          connection.query(treasureInfo, results.insertId);
-          connection.query(playerContact, results.insertId);
+          cb(results.insertId);
         }
       });
   },
